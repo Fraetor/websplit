253cd59921e0db3f88c8c2257542b5eb771c7ec6
James Frost <git@frost.cx>
2023-07-02 12:25:05 +0100

    Add stub JS functions

---
+++ websplit.js
@@ -0,0 +1,19 @@
+function hsplit() {
+    console.log("Splitting horizontally")
+}
+
+function vsplit() {
+    console.log("Splitting vertically")
+}
+
+function pop_out_split() {
+    console.log("Popping out split into new tab")
+}
+
+function close_split() {
+    console.log("Closing split")
+}
+
+function load_page() {
+    console.log("Loading page in split")
+}
